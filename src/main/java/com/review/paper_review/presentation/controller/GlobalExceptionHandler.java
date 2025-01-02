e41076b0da3f99d971a3743320b07af061ee3c75
concise123 <kang011235@gmail.com>
2025-01-02 23:38:07 +0900

    Chore: update logging configuration

--- src/main/java/com/review/paper_review/presentation/controller/GlobalExceptionHandler.java
+++ src/main/java/com/review/paper_review/presentation/controller/GlobalExceptionHandler.java
@@ -1,20 +1,18 @@
 package com.review.paper_review.presentation.controller;
 
 import com.review.paper_review.domain.exception.PostNotFoundException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@Log4j2
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
-
     @ExceptionHandler(Exception.class)
     public String handleAllExceptions(Exception ex, Model model) {
-        logger.error("오류 발생: ", ex);
+        log.error("오류 발생: ", ex);
         model.addAttribute("message", "문제가 발생했습니다.");
         return "error";
     }
